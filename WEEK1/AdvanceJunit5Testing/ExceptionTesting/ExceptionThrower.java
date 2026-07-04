d663381c812601e417e1148ba740f71f158efdbf
Gitesh Sorout <giteshsorout@gmail.com>
2026-07-04 14:00:47 +0530

    [Gitesh] Remove incorrectly pushed AdvanceJunit5Testing folder

--- WEEK1/AdvanceJunit5Testing/ExceptionTesting/ExceptionThrower.java
+++
@@ -1,8 +0,0 @@
-package WEEK1.AdvanceJunit5Testing.ExceptionTesting;
-
-public class ExceptionThrower {
-
-    public void throwException() {
-        throw new IllegalArgumentException("This is an exception");
-    }
-}
