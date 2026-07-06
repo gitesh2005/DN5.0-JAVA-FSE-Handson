82b34190adcdffdf8246a1d3907804cb13c99115
Gitesh Sorout <giteshsorout@gmail.com>
2026-07-06 09:30:32 +0530

    [Gitesh] Add : Add Exercise4 of WEEK2 of spring_core_haven

---
+++ WEEK2/Spring_Core_Maven/Exercise4/LibraryManagement/src/main/java/com/library/Main.java
@@ -0,0 +1,10 @@
+package com.library;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Library Management Maven Project Configured Successfully.");
+
+    }
+}
