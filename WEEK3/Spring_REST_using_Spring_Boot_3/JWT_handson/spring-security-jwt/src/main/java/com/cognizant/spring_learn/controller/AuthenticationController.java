e3e9bb21b25f97a0adbc4fb3b6b8ead66b8a09ce
Gitesh Sorout <giteshsorout@gmail.com>
2026-07-12 22:33:23 +0530

    [Gitesh] add : jwt_handson spring-security_jwt

--- WEEK3/Spring_REST_using_Spring_Boot_3/JWT_handson/spring-security-jwt/src/main/java/com/cognizant/spring_learn/controller/AuthenticationController.java
+++ WEEK3/Spring_REST_using_Spring_Boot_3/JWT_handson/spring-security-jwt/src/main/java/com/cognizant/spring_learn/controller/AuthenticationController.java
@@ -1,4 +1,36 @@
 package com.cognizant.spring_learn.controller;
 
+import com.cognizant.spring_learn.model.AuthenticationResponse;
+import com.cognizant.spring_learn.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@RestController
 public class AuthenticationController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/authenticate")
+    public AuthenticationResponse authenticate(HttpServletRequest request) {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        String encodedCredentials = authorizationHeader.substring("Basic ".length());
+
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedCredentials);
+
+        String decodedCredentials = new String(decodedBytes, StandardCharsets.UTF_8);
+
+        String username = decodedCredentials.split(":")[0];
+
+        String token = jwtUtil.generateToken(username);
+
+        return new AuthenticationResponse(token);
+    }
 }
--- WEEK3/Spring_REST_using_Spring_Boot_3/JWT_handson/spring-security-jwt/src/main/java/com/cognizant/spring_learn/util/JwtUtil.java
+++ WEEK3/Spring_REST_using_Spring_Boot_3/JWT_handson/spring-security-jwt/src/main/java/com/cognizant/spring_learn/util/JwtUtil.java
@@ -1,4 +1,29 @@
 package com.cognizant.spring_learn.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    private static final String SECRET =
+            "mysecretkeymysecretkeymysecretkey12";
+
+    private final SecretKey key =
+            Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(key)
+                .compact();
+    }
 }
